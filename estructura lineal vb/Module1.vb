54d2337c60b9b1d366a333d394c534e558a1facd
JoseFabela <jfabela522@gmail.com>
2023-08-30 19:00:17 -0600

    Agregar archivos de proyecto.

---
+++ estructura lineal vb/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+
+    Class BracketChecker
+        Public Shared Function AreBracketsBalanced(ByVal input As String) As Boolean
+            Dim stack As New Stack(Of Char)()
+
+            For Each ch As Char In input
+                ' Logic for bracket balancing
+            Next
+
+            Return stack.Count = 0
+        End Function
+
+        Shared Sub Main(ByVal args As String())
+            ' Input strings for testing
+            Dim input1 As String = "{[()]}"
+            Dim input2 As String = "{[(])}"
+
+            Console.WriteLine("Input 1: " & AreBracketsBalanced(input1)) ' Output: True
+            Console.WriteLine("Input 2: " & AreBracketsBalanced(input2)) ' Output: False
+        End Sub
+    End Class
+
+
+End Module
